64734cfe97c3a15b7d34a0267dfe64eb588abfd6
eccheung4 <eccheung4@gmail.com>
2021-11-18 11:13:38 -0800

    fix: simplify to get stats from polygon

--- test/polygon/pooltogether_v4.test.ts
+++ test/polygon/pooltogether_v4.test.ts
@@ -31,7 +31,7 @@ describe("PoolTogether Resolvers", () => {
         {
           forking: {
             jsonRpcUrl: `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_ID}`,
-            blockNumber: 20668404,
+            blockNumber: 21491685,
           },
         },
       ],
@@ -84,11 +84,11 @@ describe("PoolTogether Resolvers", () => {
         console.log("\t\tSymbol: ", ticketData.symbol);
         console.log("\t\tDecimals: ", ticketData.decimals.toString());
         // User balances at current block
-        console.log("\t\tUser BalanceOf: ", formatUnits(ticketData.balanceOf, ticketData.decimals));
-        console.log("\t\tUser BalanceAt eligible for prize: ", formatUnits(ticketData.balanceAt, ticketData.decimals));
-        // Total supply is total amount deposited in prize pool by everyone
-        console.log("\t\tTotal Supply/Amount deposited: ", formatUnits(ticketData.totalSupply, ticketData.decimals));
-        console.log("\t\tDelegateOf: ", ticketData.delegateOf);
+        // console.log("\t\tUser BalanceOf: ", formatUnits(ticketData.balanceOf, ticketData.decimals));
+        // console.log("\t\tUser BalanceAt eligible for prize: ", formatUnits(ticketData.balanceAt, ticketData.decimals));
+        // // Total supply is total amount deposited in prize pool by everyone
+        // console.log("\t\tTotal Supply/Amount deposited: ", formatUnits(ticketData.totalSupply, ticketData.decimals));
+        // console.log("\t\tDelegateOf: ", ticketData.delegateOf);
 
         // Draw data
         console.log("Draws: ");
@@ -104,106 +104,117 @@ describe("PoolTogether Resolvers", () => {
         // Loop through draws not including current draw
         for (let j = 0; j < draws.length; j++) {
           // Draw Data
-          console.log("\tDraw Id: ", draws[j].drawId);
-          console.log("\t\tWinning Number: ", draws[j].winningRandomNumber._hex);
-          console.log("\t\tTimeStamp: ", draws[j].timestamp.toString());
-          console.log("\t\tbeaconPeriodStartedAt: ", draws[j].beaconPeriodStartedAt.toString());
-          console.log("\t\tbeaconPeriodSeconds: ", draws[j].beaconPeriodSeconds);
+          // console.log("\tDraw Id: ", draws[j].drawId);
+          // console.log("\t\tWinning Number: ", draws[j].winningRandomNumber._hex);
+          // console.log("\t\tTimeStamp: ", draws[j].timestamp.toString());
+          // console.log("\t\tbeaconPeriodStartedAt: ", draws[j].beaconPeriodStartedAt.toString());
+          // console.log("\t\tbeaconPeriodSeconds: ", draws[j].beaconPeriodSeconds);
           // PrizeDistribution Data https://v4.docs.pooltogether.com/protocol/concepts/prize-distribution
-          console.log("\t\tPrizeDistribution: ");
-          console.log("\t\t\tBit Range Size: ", prizeDistributionsData[j].bitRangeSize);
-          console.log("\t\t\tMatch Cardinality: ", prizeDistributionsData[j].matchCardinality);
-          console.log("\t\t\tStart Timestamp Offset: ", prizeDistributionsData[j].startTimestampOffset);
-          console.log("\t\t\tEnd Timestamp Offset: ", prizeDistributionsData[j].endTimestampOffset);
-          console.log("\t\t\tMax Picks Per User: ", prizeDistributionsData[j].maxPicksPerUser);
-          console.log("\t\t\tExpiry Duration: ", prizeDistributionsData[j].expiryDuration);
-          // Number of Picks allocated to this chain based on total deposits vs all chains
-          console.log("\t\t\tNumber Of Picks: ", prizeDistributionsData[j].numberOfPicks.toString());
-          // Number of combinations/picks = (2^bit range)^cardinality
+          // console.log("\t\tPrizeDistribution: ");
           const totalNumberOfPicks =
             (2 ** prizeDistributionsData[j].bitRangeSize) ** prizeDistributionsData[j].matchCardinality;
-          console.log("\t\t\tTotal Number of Picks: ", totalNumberOfPicks);
+          console.log(
+            draws[j].drawId,
+            prizeDistributionsData[j].bitRangeSize,
+            prizeDistributionsData[j].matchCardinality,
+            prizeDistributionsData[j].numberOfPicks.toString(),
+            formatUnits(drawsData.totalSupply[j], ticketData.decimals),
+            prizeDistributionsData[j].prize.toString(),
+          );
+          // totalNumberOfPicks,
+
+          // console.log("\t\t\tMatch Cardinality: ", prizeDistributionsData[j].matchCardinality);
+          // console.log("\t\t\tStart Timestamp Offset: ", prizeDistributionsData[j].startTimestampOffset);
+          // console.log("\t\t\tEnd Timestamp Offset: ", prizeDistributionsData[j].endTimestampOffset);
+          // console.log("\t\t\tMax Picks Per User: ", prizeDistributionsData[j].maxPicksPerUser);
+          // console.log("\t\t\tExpiry Duration: ", prizeDistributionsData[j].expiryDuration);
+          // Number of Picks allocated to this chain based on total deposits vs all chains
+          // console.log("\t\t\tNumber Of Picks: ", prizeDistributionsData[j].numberOfPicks.toString());
+          // Number of combinations/picks = (2^bit range)^cardinality
+          // const totalNumberOfPicks =
+          //   (2 ** prizeDistributionsData[j].bitRangeSize) ** prizeDistributionsData[j].matchCardinality;
+          // console.log("\t\t\tTotal Number of Picks: ", totalNumberOfPicks);
           // Each prize pool is alloted a portion of the total picks proportional to its contribution to the prize network liquidity for the week.
-          console.log(
-            "\t\t\tPercentage of picks for chain: ",
-            ((prizeDistributionsData[j].numberOfPicks.toNumber() / totalNumberOfPicks) * 100).toFixed(2),
-            "%",
-          );
-          // Total Ticket TWAB Supply
-          console.log("\t\t\tAverage Total Supply TWAB: ", formatUnits(drawsData.totalSupply[j], ticketData.decimals));
-          // Price per pick = number of picks / Average Total Supply TWAB
-          console.log(
-            "\t\t\tPrice per pick: ",
-            drawsData.totalSupply[j].toNumber() / 1e6 / prizeDistributionsData[j].numberOfPicks.toNumber(),
-          );
-          console.log("\t\t\tTotal Prize Amount: ", prizeDistributionsData[j].prize.toString());
-          console.log("\t\t\tTiers: ", prizeDistributionsData[j].tiers.toString());
+          // console.log(
+          //   "\t\t\tPercentage of picks for chain: ",
+          //   ((prizeDistributionsData[j].numberOfPicks.toNumber() / totalNumberOfPicks) * 100).toFixed(2),
+          //   "%",
+          // );
+          // // Total Ticket TWAB Supply
+          // console.log("\t\t\tAverage Total Supply TWAB: ", formatUnits(drawsData.totalSupply[j], ticketData.decimals));
+          // // Price per pick = number of picks / Average Total Supply TWAB
+          // console.log(
+          //   "\t\t\tPrice per pick: ",
+          //   drawsData.totalSupply[j].toNumber() / 1e6 / prizeDistributionsData[j].numberOfPicks.toNumber(),
+          // );
+          // console.log("\t\t\tTotal Prize Amount: ", prizeDistributionsData[j].prize.toString());
+          // console.log("\t\t\tTiers: ", prizeDistributionsData[j].tiers.toString());
           const tiers = prizeDistributionsData[j].tiers;
           let numberOfPrizesForIndex = 1;
-          for (let k = 0; k < tiers.length; k++) {
-            if (tiers[k] != 0) {
-              console.log(
-                "\t\t\t\tTier Prize",
-                k,
-                ethers.BigNumber.from(tiers[k]).mul(ethers.BigNumber.from(prizeDistributionsData[j].prize)).toString(),
-              );
-              if (k > 0) {
-                // Number of prizes for a degree = (2^bit range)^degree - (2^bit range)^(degree-1)
-                numberOfPrizesForIndex =
-                  (1 << (prizeDistributionsData[j].bitRangeSize * k)) -
-                  (1 << (prizeDistributionsData[j].bitRangeSize * (k - 1)));
-              }
-              console.log("\t\t\t\t\tNumberofPrizesForIndex: ", numberOfPrizesForIndex);
-              // prize for a degree(tier) = total prize * degree percentage / number of prizes for a degree
-              const tierPrize =
-                ((prizeDistributionsData[j].prize.toNumber() / 1e9) * tiers[k]) / numberOfPrizesForIndex / 1e6;
-              console.log("\t\t\t\t\tTier Prize: $", tierPrize);
-            }
-          }
+          // for (let k = 0; k < tiers.length; k++) {
+          //   if (tiers[k] != 0) {
+          //     console.log(
+          //       "\t\t\t\tTier Prize",
+          //       k,
+          //       ethers.BigNumber.from(tiers[k]).mul(ethers.BigNumber.from(prizeDistributionsData[j].prize)).toString(),
+          //     );
+          //     if (k > 0) {
+          //       // Number of prizes for a degree = (2^bit range)^degree - (2^bit range)^(degree-1)
+          //       numberOfPrizesForIndex =
+          //         (1 << (prizeDistributionsData[j].bitRangeSize * k)) -
+          //         (1 << (prizeDistributionsData[j].bitRangeSize * (k - 1)));
+          //     }
+          //     console.log("\t\t\t\t\tNumberofPrizesForIndex: ", numberOfPrizesForIndex);
+          //     // prize for a degree(tier) = total prize * degree percentage / number of prizes for a degree
+          //     const tierPrize =
+          //       ((prizeDistributionsData[j].prize.toNumber() / 1e9) * tiers[k]) / numberOfPrizesForIndex / 1e6;
+          //     console.log("\t\t\t\t\tTier Prize: $", tierPrize);
+          //   }
+          // }
 
           // User Normalized Balances
-          console.log("\t\tUser Normalizaed Balance: ", drawsData.normalizedBalancesForDrawIds[j].toString());
-          // User Draw Picks
-          console.log("\t\tUser Draw Picks: ", drawsData.userDrawData[j].drawPicks.toString());
-          // User TWAB for this draw
-          console.log("\t\tUser BalanceAt: ", formatUnits(drawsData.userDrawData[j].balanceAt, ticketData.decimals));
-          // > 0 if user has claimed for this draw
-          console.log("\t\tUser Payout Balance: ", drawsData.userDrawData[j].payoutBalance.toString());
-          var abiEncodedValue = ethers.utils.solidityPack(
-            ["bytes32", "uint256"],
-            [ethers.utils.solidityKeccak256(["address"], [owner]), 105],
-          );
-          var userRandomNumber = ethers.utils.solidityKeccak256(["address"], [abiEncodedValue]);
+          // console.log("\t\tUser Normalizaed Balance: ", drawsData.normalizedBalancesForDrawIds[j].toString());
+          // // User Draw Picks
+          // console.log("\t\tUser Draw Picks: ", drawsData.userDrawData[j].drawPicks.toString());
+          // // User TWAB for this draw
+          // console.log("\t\tUser BalanceAt: ", formatUnits(drawsData.userDrawData[j].balanceAt, ticketData.decimals));
+          // // > 0 if user has claimed for this draw
+          // console.log("\t\tUser Payout Balance: ", drawsData.userDrawData[j].payoutBalance.toString());
+          // var abiEncodedValue = ethers.utils.solidityPack(
+          //   ["bytes32", "uint256"],
+          //   [ethers.utils.solidityKeccak256(["address"], [owner]), 105],
+          // );
+          // var userRandomNumber = ethers.utils.solidityKeccak256(["address"], [abiEncodedValue]);
 
           // Have to convert to Number because datatypes wasn't matching and got BigNumber
-          const draw = {
-            drawId: draws[j].drawId,
-            winningRandomNumber: draws[j].winningRandomNumber,
-            timestamp: draws[j].timestamp.toNumber(),
-            beaconPeriodStartedAt: draws[j].beaconPeriodStartedAt.toNumber(),
-            beaconPeriodSeconds: draws[j].beaconPeriodSeconds,
-          };
+          // const draw = {
+          //   drawId: draws[j].drawId,
+          //   winningRandomNumber: draws[j].winningRandomNumber,
+          //   timestamp: draws[j].timestamp.toNumber(),
+          //   beaconPeriodStartedAt: draws[j].beaconPeriodStartedAt.toNumber(),
+          //   beaconPeriodSeconds: draws[j].beaconPeriodSeconds,
+          // };
 
           // Calculation intensive because it must compare all picks to winningRandomNumber
-          const results = calculateDrawResults(prizeDistributionsData[j], draw, exampleUser, j);
-          console.log("\t\tUser Prizes:");
-          for (let l = 0; l < results.prizes.length; l++) {
-            console.log(
-              "\t\t\tAmount: $",
-              results.prizes[l].amount.toNumber() / 1e6,
-              " distributionIndex: ",
-              results.prizes[l].distributionIndex,
-              "pick: ",
-              results.prizes[l].pick.toString(),
-            );
-          }
-          console.log("\t\t\tTotal Value: ", results.totalValue.toNumber() / 1e6);
+          // const results = calculateDrawResults(prizeDistributionsData[j], draw, exampleUser, j);
+          // console.log("\t\tUser Prizes:");
+          // for (let l = 0; l < results.prizes.length; l++) {
+          //   console.log(
+          //     "\t\t\tAmount: $",
+          //     results.prizes[l].amount.toNumber() / 1e6,
+          //     " distributionIndex: ",
+          //     results.prizes[l].distributionIndex,
+          //     "pick: ",
+          //     results.prizes[l].pick.toString(),
+          //   );
+          // }
+          // console.log("\t\t\tTotal Value: ", results.totalValue.toNumber() / 1e6);
         }
 
-        console.log("Draw Calculator Timelock:");
-        console.log("\tTimeLock drawId: ", prizePoolData[i].timelock.drawId);
-        console.log("\tTimeLock timestamp: ", prizePoolData[i].timelock.timestamp.toString());
-        console.log("\tTimeLock hasElapsed: ", prizePoolData[i].hasElapsed);
+        // console.log("Draw Calculator Timelock:");
+        // console.log("\tTimeLock drawId: ", prizePoolData[i].timelock.drawId);
+        // console.log("\tTimeLock timestamp: ", prizePoolData[i].timelock.timestamp.toString());
+        // console.log("\tTimeLock hasElapsed: ", prizePoolData[i].hasElapsed);
 
         // Won't know user's number of picks until after prizeDistribution set, but can estimate with previous prizeDistribution
         //  assuming it stays the same and number of deposits stays around the same.
